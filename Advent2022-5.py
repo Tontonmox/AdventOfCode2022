9748ff80cb3c9f2995067cbb13cd5da3542fa5d2
Tontonmox <tontonmox@gmail.com>
2022-12-05 22:03:12 +0100

    Update Advent2022-5.py

--- Advent2022-5.py
+++ Advent2022-5.py
@@ -1,31 +1,27 @@
-f = open("Advent2022-5.txt","r")
+f = open("Advent2022-5.txt","r").read().split("\n\n")
+
+input_1 = f[0].split("\n")[:-1]
+input_2 = f[1].split("\n")[:-1]
 
 stock = [[] for i in range(9)]
 
-#Lecture des input
-for line in f:
-    #Ignorer la ligne vide
-    if len(line.strip()) == 0 : continue
+#Chargement des caisses
+for line in input_1:
+    for i in range (9):
+        if line[4*i+1] != " ":
+            stock[i].append(line[4*i+1])
 
-    #Ignorer la ligne avec les indices
-    if line[1] == '1' : continue
 
-    #Chargement des caisses
-    if line[0] != 'm':
-        for i in range (int(len(line)/4)):
-            if line[4*i+1] != " ":
-                stock[i].append(line[4*i+1])
+#Traitement des déplacements
+for line in input_2:   
+    decoup = line.split(" ")
+    num,fro,to = int(decoup[1]), int(decoup[3])-1, int(decoup[5])-1
 
-    #Traitement des déplacements
-    if line[0] == 'm':
-        decoup = line.split(" ")
-        num,fro,to = int(decoup[1]), int(decoup[3])-1, int(decoup[5])-1
-
-        #Récupération des caisses à déplacer (en les retournant)
-        move = stock[fro][:num]
-        #move.reverse()
-        del stock[fro][:num]
-        stock[to][:0] = move
+    #Récupération des caisses à déplacer (en les retournant)
+    move = stock[fro][:num]
+    #move.reverse()
+    del stock[fro][:num]
+    stock[to][:0] = move
 
 reponse_1 = ''
 for c in stock:
